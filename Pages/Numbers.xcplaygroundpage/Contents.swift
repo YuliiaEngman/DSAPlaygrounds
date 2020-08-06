6ae3cfcede74f5840a295747ba88476fac118649
Yuliia Engman <yuliia.engman@gmail.com>
2020-08-05 22:01:59 -0400

    another solution for challenge #17

--- Pages/Numbers.xcplaygroundpage/Contents.swift
+++ Pages/Numbers.xcplaygroundpage/Contents.swift
@@ -67,3 +67,37 @@ print(challenge17(input1: 1, input2: 5))
 print(challenge17(input1: 8, input2: 10))
 print(challenge17(input1: 12, input2: 12))
 print(challenge17(input1: 12, input2: 18))
+
+// Simplies solution:
+
+// However, we did not guard against negative numbers
+
+//“func challenge17a(min: Int, max: Int) -> Int {
+//    return Int.random(in: min...max)
+//}”
+//
+//Excerpt From: Paul Hudson. “Swift Coding Challenges.” Apple Books.
+
+
+/*
+ “Challenge 18: Recreate the pow() function
+ Difficulty: Easy
+
+ Create a function that accepts positive two integers, and raises the first to the power of the second.
+
+ Tip: If you name your function myPow() or challenge18(), you’ll be able to use the built-in pow() for your tests. The built-in pow() uses doubles, so you’ll need to typecast.
+
+ Sample input and output
+ The inputs 4 and 3 should return 64, i.e. 4 multiplied by itself 3 times.
+ The inputs 2 and 8 should return 256, i.e. 2 multiplied by itself 8 times.”
+
+ Excerpt From: Paul Hudson. “Swift Coding Challenges.” Apple Books.
+ */
+
+
+func challenge18(base: Int, power: Int) -> Int {
+    
+    for
+    
+    return
+}
