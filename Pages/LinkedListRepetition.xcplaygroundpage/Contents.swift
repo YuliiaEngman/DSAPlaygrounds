5b9475104ce2d1bb685046f6a06711042b875ae5
Yuliia Engman <yuliia.engman@gmail.com>
2020-08-25 23:11:18 -0400

    completed first part of challenge 43

--- Pages/LinkedListRepetition.xcplaygroundpage/Contents.swift
+++ Pages/LinkedListRepetition.xcplaygroundpage/Contents.swift
@@ -54,3 +54,133 @@ car14.next = car98
 print(car14)
 
 
+
+ class LLNode {
+   var value: Int
+   var next: LLNode?
+   init(_ value: Int) {
+     self.value = value
+   }
+ }
+ 
+ extension LLNode: CustomStringConvertible { // it will customize the description will be for us
+     var description: String {
+        // return ""
+         guard let next = next else {
+             return "\(value) -> nil" // 12 -> nil
+         }
+         // if we have connected nodes
+         return "\(value) -> \(next)" // next is a node and knows how to print itself
+     }
+ }
+ 
+ let node1 = LLNode(1)
+ let node2 = LLNode(2)
+ let node9 = LLNode(9)
+ let node4 = LLNode(4)
+ let node5 = LLNode(5)
+ 
+ node1.next = node2
+ node2.next = node9
+ node9.next = node4
+ node4.next = node5
+
+ // problem 1
+ // Using the Linked List above, write a function that finds the largest number in a Linked List of Ints.
+ // output: 9
+
+
+/*
+ func largestNumber(_ node: LLNode) -> Int {
+ //  var node = node
+ //  var largestNum = node.value
+ //
+ //  while let next = node.next {
+ //    if largestNum < next.value {
+ //      largestNum = next.value
+ //    }
+ //
+ //    node = next
+ //  }
+ //
+ //  return largestNum
+ //}
+ */
+
+/*
+“Challenge 43: Linked lists
+Difficulty: Easy
+
+Create a linked list of lowercase English alphabet letters, along with a method that traverses all nodes and prints their letters on a single line separated by spaces.”
+
+Excerpt From: Paul Hudson. “Swift Coding Challenges.” Apple Books.
+ */
+
+// I will devide this challnge for 2:
+// 1) Create a linked list to lowercased English alphabet letters
+// 2) make an extension that will print it as 1 line separated by space
+
+class AlphabetNode {
+    var value: Character
+    var next: AlphabetNode?
+    
+    init(_ value: Character) {
+        self.value = value
+    }
+}
+
+// Creating nodes
+let nodeA = AlphabetNode("a")
+let nodeB = AlphabetNode("b")
+let nodeC = AlphabetNode("c")
+let nodeD = AlphabetNode("d")
+let nodeE = AlphabetNode("e")
+let nodeF = AlphabetNode("f")
+let nodeG = AlphabetNode("g")
+let nodeH = AlphabetNode("h")
+let nodeI = AlphabetNode("i")
+let nodeJ = AlphabetNode("j")
+let nodeK = AlphabetNode("k")
+let nodeL = AlphabetNode("l")
+let nodeM = AlphabetNode("m")
+let nodeN = AlphabetNode("n")
+let nodeO = AlphabetNode("o")
+let nodeP = AlphabetNode("p")
+let nodeQ = AlphabetNode("q")
+let nodeR = AlphabetNode("r")
+let nodeS = AlphabetNode("s")
+let nodeT = AlphabetNode("t")
+let nodeU = AlphabetNode("u")
+let nodeV = AlphabetNode("v")
+let nodeW = AlphabetNode("w")
+let nodeX = AlphabetNode("x")
+let nodeY = AlphabetNode("y")
+let nodeZ = AlphabetNode("z")
+
+// Connecting nodes
+nodeA.next = nodeB
+nodeB.next = nodeC
+nodeC.next = nodeD
+nodeD.next = nodeE
+nodeE.next = nodeF
+nodeF.next = nodeG
+nodeG.next = nodeH
+nodeH.next = nodeI
+nodeI.next = nodeJ
+nodeJ.next = nodeK
+nodeK.next = nodeL
+nodeL.next = nodeM
+nodeM.next = nodeN
+nodeN.next = nodeO
+nodeO.next = nodeP
+nodeP.next = nodeQ
+nodeQ.next = nodeR
+nodeR.next = nodeS
+nodeS.next = nodeT
+nodeT.next = nodeU
+nodeU.next = nodeV
+nodeV.next = nodeW
+nodeW.next = nodeX
+nodeX.next = nodeY
+nodeY.next = nodeZ
+nodeZ.next = nil
