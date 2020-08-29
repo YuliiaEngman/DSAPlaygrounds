17684540f46763125d89ad522f0aea9314af2205
Yuliia Engman <yuliia.engman@gmail.com>
2020-08-28 22:22:07 -0400

    challenge 37

---
+++ Pages/Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ “Challenge 37: Count the numbers
+ Difficulty: Easy
+
+ Write an extension for collections of integers that returns the number of times a specific digit appears in any of its numbers.
+
+ Sample input and output
+ The code [5, 15, 55, 515].challenge37(count: "5") should return 6.
+ The code [5, 15, 55, 515].challenge37(count: "1") should return 2.
+ The code [55555].challenge37(count: "5") should return 5.
+ The code [55555].challenge37(count: "1") should return 0.”
+
+ Excerpt From: Paul Hudson. “Swift Coding Challenges.” Apple Books.
+ */
+
+func challenge37(input1: [Int], input2: Int) -> Int? {
+    
+    //let strFromArr: String = input1
+    //let oneStr = strFromArr.replacingOccurrences(of: ", ", with: "")
+    
+    return 0
+}
