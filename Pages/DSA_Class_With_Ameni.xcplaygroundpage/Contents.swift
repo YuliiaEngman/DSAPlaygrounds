8b369792f2ede2c87460da9de0e17024fdcde3d2
Yuliia Engman <yuliia.engman@gmail.com>
2020-09-12 23:50:43 -0400

    second DSA problem

--- Pages/DSA_Class_With_Ameni.xcplaygroundpage/Contents.swift
+++ Pages/DSA_Class_With_Ameni.xcplaygroundpage/Contents.swift
@@ -86,3 +86,25 @@ class Solution {
         
     }
 }
+
+
+
+/*
+ MatrixSum
+
+ Difficulty    Tags    Solution Link
+ Easy    [Arrays, Matrices, Integers]    To Do
+ Problem Description
+
+ Find the sum of all elements in a two dimensional, rectangular array except for the corners.
+
+ Sample
+
+ Input    Output
+ 1 3 9 2
+ 3 2 0 3
+ 2 8 1 4
+ */
+
+
+
