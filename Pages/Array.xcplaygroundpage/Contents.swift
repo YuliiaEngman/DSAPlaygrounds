255031ccba2721e1d809548d976b93b609ee0691
Yuliia Engman <yuliia.engman@gmail.com>
2020-09-18 23:08:02 -0400

    challenge 88 from Leetcode

--- Pages/Array.xcplaygroundpage/Contents.swift
+++ Pages/Array.xcplaygroundpage/Contents.swift
@@ -88,3 +88,36 @@ func currentCohort(_ cohort: Double = 6.0) {
 
 currentCohort() // current cohort is 6.0
 currentCohort(7.0) // current cohort is 7.0
+
+
+// Leetcode problem
+// Homework - review inout parameters
+
+/*
+ Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+ Note:
+
+ The number of elements initialized in nums1 and nums2 are m and n respectively.
+ You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+ Example:
+
+ Input:
+ nums1 = [1,2,3,0,0,0], m = 3
+ nums2 = [2,5,6],       n = 3
+
+ Output: [1,2,2,3,5,6]
+ */
+
+
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        
+        for num in nums2 {
+            nums1.append(num)
+        }
+       nums1.sorted()
+    }
+    
+    var input = [1,2,3,0,0,0]
+    print(merge(&input, 3, [2,5,6], 3))
+
