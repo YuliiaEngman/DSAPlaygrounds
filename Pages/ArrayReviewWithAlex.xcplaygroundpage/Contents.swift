43c407318b9c1142c038bb6ae30744197e956f25
Yuliia Engman <yuliia.engman@gmail.com>
2020-09-23 21:56:32 -0400

    added notes

--- Pages/ArrayReviewWithAlex.xcplaygroundpage/Contents.swift
+++ Pages/ArrayReviewWithAlex.xcplaygroundpage/Contents.swift
@@ -72,7 +72,11 @@ let heteroheneousArray: [Any] = [1, 2, 4.7, "Greg", true]
 Challenge 88:
  Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
-/*
+ Note:
+
+ The number of elements initialized in nums1 and nums2 are m and n respectively.
+ You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+ */
 
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
