21e83440748ef9a3f46ffb09c09bc94c7367d5b0
Yuliia Engman <yuliia.engman@gmail.com>
2020-09-24 23:26:07 -0400

    shift zeros challenge

--- Pages/ArrayReviewWithAlex.xcplaygroundpage/Contents.swift
+++ Pages/ArrayReviewWithAlex.xcplaygroundpage/Contents.swift
@@ -191,5 +191,27 @@ merge(&firstArr3, 3, [2,5,6], 3)
 
 
 
+
+
+/*
+ DSA for Class with Ameni
+ ShiftZeros
+
+ Difficulty    Tags    Solution Link
+ Medium    [Arrays, Duplicates]    To Do
+ Problem Description
+
+ Given an array of numbers, reset the array to put all the non-zero numbers in front of all the zeros in the array, then return the new array and the count of non-zero numbers.
+
+ Sample
+
+ Input    Output
+ [3,0,2,0,0,1,0,4]    [3,2,1,4,0,0,0,0], 4
+ */
+
+
+
+
+
  
 
