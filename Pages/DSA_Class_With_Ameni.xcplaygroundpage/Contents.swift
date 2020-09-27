3bfb0d0025cd968d21b4bf40762433e6feca37b3
Yuliia Engman <yuliia.engman@gmail.com>
2020-09-26 23:50:42 -0400

    new challenge - merge sorting arrays

--- Pages/DSA_Class_With_Ameni.xcplaygroundpage/Contents.swift
+++ Pages/DSA_Class_With_Ameni.xcplaygroundpage/Contents.swift
@@ -410,6 +410,26 @@ func getDiagnolSum(_ matrix: [[Int]]) -> Int {
  */
 
 
+// Arrays
+/*
+ MergeSortedArrays
+
+ Difficulty    Tags    Solution Link
+ Medium    [Arrays]    To Do
+ Problem Description
+
+ Given two sorted arrays, write a function that returns a single sorted list with all the elements from the inputs. Your solution must work in O(n) time.
+
+ Sample
+
+ Input    Output
+ [3, 4, 6, 10, 11, 15]
+ [1, 5, 8, 12, 14, 19, 31]    [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 31]
+ */
+
+
+
+
 
 
 
