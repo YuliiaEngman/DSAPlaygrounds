575d45c65e600c706bff4f0b07b66feeaccd3893
Yuliia Engman <yuliia.engman@gmail.com>
2020-09-29 23:47:20 -0400

    character challenge

---
+++ Pages/Character.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+387. First Unique Character in a String
+Easy
+
+2229
+
+122
+
+Add to List
+
+Share
+Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode"
+return 2.
+ 
+
+Note: You may assume the string contains only lowercase English letters.
+ */
+
+
+
+
