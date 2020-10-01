85db38a315bbdeee8d3ca710f0db532e4b7af2d8
Yuliia Engman <yuliia.engman@gmail.com>
2020-09-30 23:53:10 -0400

    common char challenge

--- Pages/Character.xcplaygroundpage/Contents.swift
+++ Pages/Character.xcplaygroundpage/Contents.swift
@@ -28,5 +28,45 @@ Note: You may assume the string contains only lowercase English letters.
  */
 
 
+func firstUniqChar(_ s: String) -> Int {
+    
+    let uniqStr = s.un
+    
+    for char in
+    
+}
+
+/*
+ y
+
+ 1057
+
+ 119
+
+ Add to List
+
+ Share
+ Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).  For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
+
+ You may return the answer in any order.
+
+  
+
+ Example 1:
+
+ Input: ["bella","label","roller"]
+ Output: ["e","l","l"]
+ Example 2:
+
+ Input: ["cool","lock","cook"]
+ Output: ["c","o"]
+ */
+
+class Solution {
+    func commonChars(_ A: [String]) -> [String] {
+        
+    }
+}
+
 
 
