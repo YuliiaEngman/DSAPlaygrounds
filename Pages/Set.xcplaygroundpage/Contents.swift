019f77d59da1aa8702ce53c57b6d4bd3798d2cfe
Yuliia Engman <yuliia.engman@gmail.com>
2020-10-07 23:06:23 -0400

    think on correct solution

--- Pages/Set.xcplaygroundpage/Contents.swift
+++ Pages/Set.xcplaygroundpage/Contents.swift
@@ -38,5 +38,55 @@ Output: true
 // 2) And compare values.count of dictionary to the set of thouse values - if it equal, return true
     func uniqueOccurrences(_ arr: [Int]) -> Bool {
         
+       // 1) I will loop trough array and create a frequancy dictionary (put number and show how many time it occures)
+        var dictionary = Dictionary<Int, Int>()
+        
+        for num in arr {
+            
+            if let numCount = dictionary[num] {
+                dictionary[num] = numCount + 1
+            } else {
+                dictionary[num] = 1
+            }
+        }
+        
+        print(dictionary)
+        print(dictionary.count)
+        
+        //2) And compare values.count of dictionary to the set of thouse values - if it equal, return true
+        
+        let setArr = Set(arr)
+        
+        return setArr.count == dictionary.values.count
     }
 
+print(uniqueOccurrences([1,2,2,1,1,3])) //true
+print(uniqueOccurrences([1,2]))
+
+
+/*
+ func numberOfVowels(_ str: String) -> [String:Int] {
+     guard !str.isEmpty else { return [:] }
+     
+     var vowelDict = Dictionary<String,Int>()
+     let vowelSet: Set<String> = ["a","e","i","o","u"]
+     
+     for char in str.lowercased() {
+         
+         if vowelSet.contains(String(char)) {
+
+             if let charCount = vowelDict[String(char)] {
+                 // Increment the value for the key in the dictionary.
+                 vowelDict[String(char)] = charCount + 1
+             } else {
+                 // Map a value to this key.
+                 vowelDict[String(char)] = 1
+             }
+             
+         }
+         
+     }
+     
+     return vowelDict
+ }
+ */
