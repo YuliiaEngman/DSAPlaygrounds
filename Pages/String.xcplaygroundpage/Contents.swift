25da1ff3f5c74b955d060ff6a83d5a9627a1a052
Yuliia Engman <yuliia.engman@gmail.com>
2020-10-12 23:09:21 -0400

    easiest way to solve the challenge using built-in function

--- Pages/String.xcplaygroundpage/Contents.swift
+++ Pages/String.xcplaygroundpage/Contents.swift
@@ -3,7 +3,6 @@
 import Foundation
 
 /*
-
  Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
 
  Each letter in the magazine string can only be used once in your ransom note.
@@ -23,3 +22,14 @@ import Foundation
  Input: ransomNote = "aa", magazine = "aab"
  Output: true
  */
+
+// Sudocode:
+// the easiest way to check I think would be build-in func .contains
+
+func noteContains(_ ransomNote: String, _ magazine: String) -> Bool {
+    return magazine.contains(ransomNote)
+}
+
+print(noteContains("a", "b")) // false
+print(noteContains("aa", "ab")) // false
+print(noteContains("aa", "aab")) // true
