93952899f235b8fec3b844a94c10c263514d4db1
Yuliia Engman <yuliia.engman@gmail.com>
2020-10-12 23:17:11 -0400

    new Swift challenge

--- Pages/String.xcplaygroundpage/Contents.swift
+++ Pages/String.xcplaygroundpage/Contents.swift
@@ -33,3 +33,14 @@ func noteContains(_ ransomNote: String, _ magazine: String) -> Bool {
 print(noteContains("a", "b")) // false
 print(noteContains("aa", "ab")) // false
 print(noteContains("aa", "aab")) // true
+print(noteContains("aa", "baa")) // true
+
+/*
+ Roy wanted to increase his typing speed for programming contests. His friend suggested that he type the sentence
+ "The quick brown fox jumps over the lazy dog" repeatedly. This sentence is known as a pangram because it contains
+ every letter of the alphabet.
+
+ After typing the sentence several times, Roy became bored with it so he started to look for other pangrams.
+
+ Given a sentence, determine whether it is a pangram. Ignore case.
+ */
